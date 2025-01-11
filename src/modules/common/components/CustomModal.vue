c67ae55b272a0ac6390a02b9697f990a849c1298
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-11 17:01:56 -0500

    custom modal with slot and names

---
+++ src/modules/common/components/CustomModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <dialog class="modal" :open="modalOpen">
+    <div class="modal-box">
+
+      <div class="border-b border-blue-500">
+        <!-- Header -->
+        <slot name="header"/>
+      </div>
+      <div class="my-5">
+        <!-- Content -->
+        <slot name="body"/>
+      </div>
+      <div class="border-t border-t-blue-500 pt-2">
+        <slot name="footer"/>
+      </div>
+    </div>
+  </dialog>
+  <div v-if="modalOpen" class="modal-backdrop fixed top-0 left-0 z-10 bg-black opacity-50 w-screen h-screen"></div>
+</template>
+
+<script lang="ts" setup>
+  interface Props {
+    modalOpen: boolean;
+  }
+
+  withDefaults(defineProps<Props>(), {
+    modalOpen: false,
+  });
+</script>
--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -26,9 +26,27 @@
       :modal-open="modalOpen" 
       @close="modalOpen = false" 
       @value="onNewValue"/>
+    <custom-modal 
+      :modal-open="customModalOpen" >
+      <template #header>
+        <h1 class="text-2xl ">Hello</h1>
+      </template>
+      <template #body>
+        <p>Consectetur laboris officia tempor incididunt fugiat irure ea do amet ad consectetur pariatur dolore. Et qui nisi cillum anim enim ipsum eiusmod cupidatat do id. Anim sunt aute labore deserunt nisi velit officia. Esse commodo velit aliqua sint. Amet excepteur adipisicing irure fugiat aute dolor pariatur incididunt dolore eiusmod. Ipsum non non labore esse.</p>
+      </template>
+      <template #footer>
+        <div class="flex justify-end">
+          <button class="btn mr-4" @click="customModalOpen = false">Close</button>
+          <button class="btn btn-primary">Save</button>
+        </div>
+      </template>
+    </custom-modal>
 
     <fab-button @click="modalOpen = true">
       <AddCircle/> 
+    </fab-button>
+    <fab-button position="bottom-left" @click="customModalOpen = true">
+      <AddProject/> 
     </fab-button>
   </div>
 </template>
@@ -38,8 +56,11 @@
   import FabButton from '@/modules/common/components/FabButton.vue'
   import InputModal from '@/modules/common/components/InputModal.vue';
   import AddCircle from '@/modules/common/icons/AddCircle.vue';
+  import CustomModal from '@/modules/common/components/CustomModal.vue';
+  import AddProject from '@/modules/common/icons/AddProject.vue';
 
   const modalOpen = ref(false);
+  const customModalOpen = ref(false);
 
   const onNewValue = (projectName: string) => {
     console.log('<--------------- JK ProjectsView --------------->');
