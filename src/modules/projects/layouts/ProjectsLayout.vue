279a9bf8b753f77e3d8605f977202b97617d7e15
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-10 22:39:49 -0500

    first commit

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,13 @@
+import { RouterView } from 'vue-router';
+<template>
+  <div class="flex flex-col">
+    <!-- Top menu -->
+    <main>
+      <!-- Side menu -->
+
+      <!-- Router view -->
+      <RouterView/>
+
+    </main>
+  </div>
+</template>
