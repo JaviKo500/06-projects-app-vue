fc18d7fa0c8c84a046a44724d09e29c740b5dd83
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-10 22:57:50 -0500

    menu styles

---
+++ src/modules/common/components/TopMenu.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex justify-between items-center p-4 bg-base-200">
+    <h2 class="text-lg font-bold text-blue-600">Fail Notion</h2>
+    
+  </div>
+</template>
--- src/modules/projects/layouts/ProjectsLayout.vue
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -1,13 +1,17 @@
-import { RouterView } from 'vue-router';
 <template>
   <div class="flex flex-col">
     <!-- Top menu -->
+    <top-menu></top-menu>
     <main>
       <!-- Side menu -->
-
+      <side-menu></side-menu>
       <!-- Router view -->
-      <RouterView/>
+      <router-view></router-view>
 
     </main>
   </div>
 </template>
+<script setup lang="ts">
+  import TopMenu from '@/modules/common/components/TopMenu.vue';
+  import SideMenu from '@/modules/projects/components/SideMenu.vue';
+</script>
