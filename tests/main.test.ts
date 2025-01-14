869746afd8c92d6a19c2378fba60b3c47d6fe336
JaViKo500 <javikogutierrez64@gmail.com>
2025-01-13 22:13:37 -0500

    test main ts

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,34 @@
+
+vi.mock('@/router', () => ({
+  default: 'router',
+}));
+
+vi.mock('pinia', async ( importOriginal ) => {
+  const mod = await importOriginal() ?? {};
+
+  return {
+    ...mod,
+    createPinia: vi.fn().mockReturnValue('pinia'),
+  };
+});
 describe('Main.test', () => {
-   test( 'should be configured as default', () => {
-      expect(1).toBe(1);
-   });
+  // eslint-disable-next-line
+  const vue = require('vue');
+  const useSpy = vi.fn();
+  const mountSoy = vi.fn();
+  const createApp = vi.fn().mockReturnValue({
+    use: useSpy,
+    mount: mountSoy,
+  });
+
+  vue.createApp = createApp;
+
+  test( 'should be configured with pinia and router', async () => {
+    await import('@/main.ts');
+    expect(vue.createApp).toHaveBeenCalled();
+    expect(mountSoy).toHaveBeenCalledWith('#app');
+
+    expect(useSpy).toHaveBeenCalledWith('router');
+    expect(useSpy).toHaveBeenCalledWith('pinia');
+  });
 });
